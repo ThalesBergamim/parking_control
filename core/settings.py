4727dba686bfaf9a02c8024568e96977f3602a35
Thales Bergamin <thalesbergamim@gmail.com>
2025-04-10 22:59:21 -0300

    add dockerfile and docker-compose

--- core/settings.py
+++ core/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': BASE_DIR / 'parking_service',
+        'USER': 'postgres',
+        'PASSWORD': 'temp@0209',
+        'HOST': 'parking_db',
+        'PORT': '5432',
     }
 }
 
